3c6de18b38cfc49bbc88ef27777b9056f3864124
Sébastien Willmann <sebastien.willmann@gmail.com>
2015-11-11 12:32:28 +0100

    Read multiple products

--- fnag/src/main/java/willmann/sebastien/fnag/ProductReader.java
+++ fnag/src/main/java/willmann/sebastien/fnag/ProductReader.java
@@ -1,11 +1,41 @@
 package willmann.sebastien.fnag;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
 
+/**
+ * This class contains static methods to read {@link Product} objects from input
+ *
+ */
 public final class ProductReader {
 	
 	private final static String SEPARATOR = "\\|";
 	
+	/**
+	 * Reads products from the specified {@link BufferedReader}.
+	 * The first line must contain the number of products to read.
+	 * 
+	 * @param reader A buffered reader to read from
+	 * @return a set containing the products
+	 * @throws IOException
+	 */
+	public static Set<Product> readProductSet(BufferedReader reader) throws IOException {
+		int linesNumber = Integer.parseInt(reader.readLine());
+		Set<Product> result = new HashSet<>();
+		for(int i = 0; i < linesNumber; i++) {
+			result.add(readProduct(reader.readLine()));
+		}
+		return result;
+	}
+	
+	/**
+	 * Reads a single line
+	 * @param line
+	 * @return the product
+	 */
 	public static Product readProduct(String line) {
 		String splitLine[] = line.split(SEPARATOR);
 		return new Product(splitLine[0], new BigDecimal(splitLine[1]), splitLine[2]);
--- fnag/src/test/java/willmann/sebastien/fnag/ProductReaderTest.java
+++ fnag/src/test/java/willmann/sebastien/fnag/ProductReaderTest.java
@@ -2,7 +2,11 @@ package willmann.sebastien.fnag;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
 import java.math.BigDecimal;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -20,6 +24,18 @@ public class ProductReaderTest {
 		assertEquals("Foo", product.getReference());
 		assertEquals(new BigDecimal("12.34"), product.getPrice());
 		assertEquals("A test product", product.getDescription());
+	}
+	
+	@Test
+	public void readProductSet() throws IOException {
+		String input = "3\n"
+				+ "Foo|12.34|A test product\n"
+				+ "Bar|42|Second product\n"
+				+ "Baz|22.2|Another one";
+		
+		Set<Product> result = ProductReader.readProductSet(new BufferedReader(new StringReader(input)));
+		
+		assertEquals(3, result.size());
 	}
 
 }
